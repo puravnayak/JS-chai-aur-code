e26189f294108565befad6201fff57872799f173
puravnayak <143780759+puravnayak@users.noreply.github.com>
2024-10-21 19:35:41 +0000

    commit 1

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+/*
+    1. Using semicolon in JS is optional but recommended
+    2. Never use var as it has access specificer problem and always use let instead
+    3. A variable as a undefined value by default until you change it
+
+*/
+
+
+const accountId = 144553
+let accountEmail = "purav@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId=2;
+accountEmail="pjn@gmail.com"
+accountPassword="123"
+accountCity="Bangalore"
+
+// console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity]);
